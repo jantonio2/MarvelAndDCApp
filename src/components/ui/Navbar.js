e057bd986cf1ff25ea091e65b3d4b8a16a3adba5
jantonio2 <jesusrochavedia@gmail.com>
2021-11-03 19:12:20 -0400

    Creado el AppRouter central y corregidas las dependencias de react-router-dom

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,46 +1,45 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
-      <Link className='navbar-brand' to='/'>
-        Asociaciones
-      </Link>
-
-      <div className='navbar-collapse'>
-        <div className='navbar-nav'>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/marvel'
-          >
-            Marvel
-          </NavLink>
-
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/dc'
-          >
-            DC
-          </NavLink>
+    <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <div class='container-fluid'>
+        <NavLink to='/' className='navbar-brand'>
+          ASOCIACIONES
+        </NavLink>
+        <button
+          class='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarText'
+          aria-controls='navbarText'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class='collapse navbar-collapse' id='navbarText'>
+          <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li class='nav-item'>
+              <NavLink to='/marvel' className='nav-link'>
+                Marvel
+              </NavLink>
+            </li>
+            <li class='nav-item'>
+              <NavLink to='/dc' className='nav-link'>
+                DC
+              </NavLink>
+            </li>
+          </ul>
+          <ul class='navbar-nav'>
+            <li class='nav-item'>
+              <NavLink to='/login' className='nav-link'>
+                Logout
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </div>
-
-      <div className='navbar-collapse collapse w-100 order-3 dual-collapse2'>
-        <ul className='navbar-nav ml-auto'>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/login'
-          >
-            Logout
-          </NavLink>
-        </ul>
       </div>
     </nav>
   );
