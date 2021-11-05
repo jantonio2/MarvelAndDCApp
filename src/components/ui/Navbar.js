5be0ee862fb047f32c4472433d48eefdb02c3805
jantonio2 <jesusrochavedia@gmail.com>
2021-11-05 17:56:23 -0400

    Segundo Router añadido

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
-    <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <div class='container-fluid'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <div className='container-fluid'>
         <NavLink to='/' className='navbar-brand'>
           ASOCIACIONES
         </NavLink>
         <button
-          class='navbar-toggler'
+          className='navbar-toggler'
           type='button'
           data-bs-toggle='collapse'
           data-bs-target='#navbarText'
@@ -17,23 +17,23 @@ export const Navbar = () => {
           aria-expanded='false'
           aria-label='Toggle navigation'
         >
-          <span class='navbar-toggler-icon'></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div class='collapse navbar-collapse' id='navbarText'>
-          <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li class='nav-item'>
+        <div className='collapse navbar-collapse' id='navbarText'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+            <li className='nav-item'>
               <NavLink to='/marvel' className='nav-link'>
                 Marvel
               </NavLink>
             </li>
-            <li class='nav-item'>
+            <li className='nav-item'>
               <NavLink to='/dc' className='nav-link'>
                 DC
               </NavLink>
             </li>
           </ul>
-          <ul class='navbar-nav'>
-            <li class='nav-item'>
+          <ul className='navbar-nav'>
+            <li className='nav-item'>
               <NavLink to='/login' className='nav-link'>
                 Logout
               </NavLink>
