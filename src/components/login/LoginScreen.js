6b47cfd41a1cfee4868f4b9294e43b556070bfd4
jantonio2 <jesusrochavedia@gmail.com>
2021-11-05 20:18:09 -0400

    Uso de useHistory, push y replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 export const LoginScreen = () => {
+  // Definir (En la documentacion esta como let)
+  const history = useHistory();
+
+  const handleLogin = () => {
+    // history.push('/');
+    history.replace('/');
+  }
+
   return (
-    <div>
-      <h1>Login Screen</h1>
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button
+        className="btn btn-primary"
+        onClick={handleLogin}
+      >
+        Ingresar
+      </button>
     </div>
-  );
-};
+  )
+}
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -42,5 +42,5 @@ export const Navbar = () => {
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
