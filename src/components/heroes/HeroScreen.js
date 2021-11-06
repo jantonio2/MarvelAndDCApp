7b79f88c9792f53beb0d93670ed716dfdff0a62d
jantonio2 <jesusrochavedia@gmail.com>
2021-11-06 11:39:31 -0400

    Agregando estilos al componente HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,27 +1,60 @@
 import React from 'react'
-import { useParams, Redirect } from 'react-router-dom'
+import { useParams, Redirect, useHistory } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById'
 
 export const HeroScreen = () => {
 
   const {heroId} = useParams()
-
   const hero = getHeroById(heroId)
+  let history = useHistory();
 
   if(!hero){
     return <Redirect to="/" />
   }
 
-  const {id,
-        superhero,
+  const {superhero,
         publisher,
         alter_ego,
         first_appearance,
         characters} = hero
 
+  const handleReturn = () => {
+    if(history.length <= 2){
+      publisher === 'Marvel Comics' && history.push('/')
+      publisher === 'DC Comics' && history.push('/dc')
+    } else {
+      history.goBack()
+    }
+  }  
+  
   return (
-    <div>
-      <h1>Hero Screen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={superhero}
+          className="img-thumbnail"
+        />
+      </div>
+
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b>{alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b>{publisher}</li>
+          <li className="list-group-item"><b>First appearance: </b>{first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-4">Characters</h5>
+        <p>{characters}</p>
+
+        <button 
+          className="btn btn-outline-info"
+          onClick={handleReturn}
+        >
+          Return
+        </button>
+      </div>
     </div>
   )
 }
